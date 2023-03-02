127636a9462bb916ca8891fe75856e3ad4f67300
Mary-Ann Johanna Frunzete <maryannfrunzete@gmail.com>
2023-03-02 19:58:53 +0200

    return json from deleteTeamRequest

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ function deleteTeamRequest(id) {
       "Content-Type": "application/json"
     },
     body: JSON.stringify({ id })
-  });
+  }).then(r => r.json());
 }
 
 function displayTeams(teams) {
@@ -67,10 +67,7 @@ function initEvents() {
   document.querySelector("#teams tbody").addEventListener("click", e => {
     if (e.target.matches("a")) {
       const id = e.target.dataset.id;
-      const p = deleteTeamRequest(id);
-      console.warn("p", p);
-      p.then(r => r.json()).then(status => {
-        console.info("s", status);
+      deleteTeamRequest(id).then(status => {
         if (status.success) {
           window.location.reload();
         }
