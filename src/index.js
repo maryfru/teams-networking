285a866ae3c92b5cca59893319349971c50d02ad
Mary-Ann Johanna Frunzete <maryannfrunzete@gmail.com>
2023-04-05 20:59:04 +0300

    await deleteTeamRequest

--- src/index.js
+++ src/index.js
@@ -155,15 +155,14 @@ function initEvents() {
     editId = undefined;
   });
 
-  document.querySelector("#teams tbody").addEventListener("click", e => {
+  document.querySelector("#teams tbody").addEventListener("click", async e => {
     if (e.target.matches("a.remove-btn")) {
       const id = e.target.dataset.id;
-      deleteTeamRequest(id).then(status => {
-        if (status.success) {
-          loadTeams();
-          // TODO don't load all teams...
-        }
-      });
+      const status = await deleteTeamRequest(id);
+      if (status.success) {
+        loadTeams();
+        // TODO homework: don't load all teams...
+      }
     } else if (e.target.matches("a.edit-btn")) {
       const id = e.target.dataset.id;
       prepareEdit(id);
